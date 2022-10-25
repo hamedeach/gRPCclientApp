1d214eb67e8b04bcfe912dfa483d7c6ee630ea87
Mohamed Hamed <44405928+hamedeach@users.noreply.github.com>
2022-10-25 15:20:31 +0200

    consume a gRPC

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Grpc.Net.Client;
+using gRPCService;
+
+Console.WriteLine("##### gRPC Client APP ######");
+var channel = GrpcChannel.ForAddress("https://localhost:5001");
+var client = new Greeter.GreeterClient(channel);
+
+var response = await client.SayHelloAsync(
+    new HelloRequest { Name = " World" });
+
+Console.WriteLine(response.Message);
+
+var responsebye = await client.sayByeAsync(
+    new HelloRequest { Name = " World" });
+
+Console.WriteLine(responsebye.Byemessage);
+
+Console.ReadKey();
